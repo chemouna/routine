7e0cd3285685b5724c89fee65465209c6c218795
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-21 15:46:55 +0100

    Add some input validation.

--- src/routine/core.clj
+++ src/routine/core.clj
@@ -1,17 +1,28 @@
 (ns routine.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
-(defprotocol Validatable
+(defprotocol ValidatableTask
   "A protocol to define a task that can be in a workflow"
   (validate [this] "Validate the task"))
 
 ;; maybe validate method here shld be a multimethod
 
 (defrecord WorkflowTask [name ignore_errors]
-  Validatable
-  )
+  ValidatableTask)
 
-(def task1 (->WorkflowTask "task 1" false))
+(defn workflowTask
+  "Create a new workflow task."
+  [{:keys [name ignore_errors]}]
+  {:pre [(string? name)
+         (blank? name)]}
+  (->WorkflowTask name ignore_errors))
+
+(def task1 (workflowTask "task 1" true))
+
+(map->WorkflowTask {:name "task2" :ignore_errors false  :name "task3" :ignore_errors true})
+
+(:ignore_errors task)
 
 (defn -main
   "I don't do a whole lot ... yet."
