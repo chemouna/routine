824dde1dda07c528bff0721e1085e2f07c103dde
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-21 17:32:41 +0100

    Add defrecord ctor.

--- src/routine/core.clj
+++ src/routine/core.clj
@@ -1,28 +1,25 @@
 (ns routine.core
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as string]))
 
-(defprotocol ValidatableTask
+(defprotocol Validatable
   "A protocol to define a task that can be in a workflow"
   (validate [this] "Validate the task"))
 
 ;; maybe validate method here shld be a multimethod
 
 (defrecord WorkflowTask [name ignore_errors]
-  ValidatableTask)
+  Validatable)
 
-(defn workflowTask
+(defn workflowTask [name ignore_errors]
   "Create a new workflow task."
-  [{:keys [name ignore_errors]}]
   {:pre [(string? name)
-         (blank? name)]}
+         (not (string/blank? name))]}
   (->WorkflowTask name ignore_errors))
 
 (def task1 (workflowTask "task 1" true))
 
-(map->WorkflowTask {:name "task2" :ignore_errors false  :name "task3" :ignore_errors true})
-
-(:ignore_errors task)
+(:ignore_errors task1)
 
 (defn -main
   "I don't do a whole lot ... yet."
